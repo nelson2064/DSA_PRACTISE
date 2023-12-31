d03fa90c363ae7867f941c39fa551d643aedb87a
nelson parajuli <nelsonparajuli2064@gmail.com>
2024-01-01 04:53:17 +0545

    sort code

---
+++ ArrayList/Sort.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+
+public class Sort{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+        Collections.sort(list , Collections.reverseOrder());
+        System.out.println(list);
+    }
+}
