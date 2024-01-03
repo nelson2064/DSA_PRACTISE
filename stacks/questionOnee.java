527d4ab87a405f6aecf43a3ef517bf5e4c2ba748
nelson parajuli <nelsonparajuli2064@gmail.com>
2024-01-03 21:25:17 +0545

    push at bottom of the stack

---
+++ stacks/questionOnee.java
@@ -0,0 +1,25 @@
+package stacks;
+import java.util.*;
+public class questionOnee {
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return; 
+        }
+
+        int temp = s.pop();
+        pushAtBottom(s, data);
+        s.push(temp);
+    }
+  public static void main(String[] args) {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    pushAtBottom(stack, 4);
+
+    while(!stack.isEmpty()) {
+        System.out.println(stack.pop());
+    }
+  }  
+}
