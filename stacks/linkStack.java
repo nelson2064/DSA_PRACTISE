02bb4ee9406125fa90f1638e201acb93f5aacca8
nelson parajuli <nelsonparajuli2064@gmail.com>
2024-01-03 21:14:37 +0545

    using

--- stacks/linkStack.java
+++ stacks/linkStack.java
@@ -48,5 +48,16 @@ public class linkStack {
         }
     }
 
-   
+    public static void main(String args[]) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
 }
