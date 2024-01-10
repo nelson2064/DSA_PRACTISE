5f71ec2b6fb971fc55252375cf85ff9058edc62a
nelson parajuli <nelsonparajuli2064@gmail.com>
2024-01-10 11:40:59 +0545

    question misunderstanding

--- stacks/questionTwo.java
+++ stacks/questionTwo.java
@@ -2,23 +2,34 @@ package stacks;
 import java.util.*;
 public class questionTwo {
     
-    public static void reverse(Stack<Integer> s ){
-        if(s.isEmpty()){
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if(s.isEmpty()) {
+            s.push(data);
             return;
         }
 
-        int rm = s.pop();
-        reverse(s);
-        s.push(rm);
+        int temp = s.pop();
+        pushAtBottom(s, data);
+        s.push(temp);
     }
-    
-    public static void main(String[] args) {
+
+    public static void reverse(Stack<Integer> s) {
+        if(s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void main(String args[]) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
         reverse(stack);
-    
+
         while(!stack.isEmpty()) {
             System.out.println(stack.pop());
         }
