de2360d10b052d57b9dcea7e286e53e7bd91a1ee
nelson parajuli <nelsonparajuli2064@gmail.com>
2024-01-14 11:56:28 +0545

    testing queue though array

--- Queues/ArrayQueues.java
+++ Queues/ArrayQueues.java
@@ -54,6 +54,15 @@ public class ArrayQueues {
    }
 
    public static void main(String[] args) {
-    
+    Queue q = new Queue(5);
+    q.add(1);
+    q.add(2);
+    q.add(3);
+
+    while(!q.isEmpty()){
+        System.out.println(q.peek());
+        q.remove();
+    }
+
    }
 }
