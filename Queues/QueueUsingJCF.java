6ab5634337cded32fa2662c3eaed2d8ca59d1272
nelson parajuli <nelsonparajuli2064@gmail.com>
2024-01-14 12:46:46 +0545

    jfc for queue

---
+++ Queues/QueueUsingJCF.java
@@ -0,0 +1,8 @@
+package Queues;
+import java.util.*;
+
+public class QueueUsingJCF {
+    Queue<Integer> q = new LinkedList<>(); //ArrayDeque
+    //Queue is an interface so we cannot create an object of interface 
+    //in java we can implment queue using linkedlist and arraydeque
+}
